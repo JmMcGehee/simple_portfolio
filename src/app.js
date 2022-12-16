e88ddafecbbef0ce955eb35dc6528ef03c9627f8
Joshua McGehee <mcgehee.joshua@gmail.com>
2022-12-16 10:43:09 -0700

    added js to deal with ericjones'

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
 const aboutMeButton = document.getElementById('about-me-button')
 
+const email = document.getElementById('email-input')
+const message = document.getElementById('message-input')
+const submitButton = document.getElementById('submit-button')
+
 function toggleButton() {
   navList.classList.toggle('show')
 }
@@ -12,3 +16,21 @@ function goToAboutMe() {
 
 hamburgerButton.addEventListener('click', toggleButton)
 aboutMeButton.addEventListener('click', goToAboutMe)
+submitButton.addEventListener('click', checkEmail)
+
+//form onSubmit....set on the submit button onClick?? 
+//look at the email input
+//If the email is 'ericjonesmyemail@gmail.com'
+//Change the message to 'Eric Jones, go fuck yourself'
+//Send to ericjonesmyemail@gmail.com
+//else, form action="https://formsubmit.co/b5401f0a7ff5ea4761953ab50381d0ed "
+
+function checkEmail() {
+  if (email.value === 'ericjonesmyemail@gmail.com') {
+    message.value = "Eric Jones, fuck off."
+    document.contact_form.action = "https://formsubmit.co/ericjonesmyemail@gmail.com"
+    console.log(email.value + " is a bastard")
+  } else {
+    document.contact_form.action = "https://formsubmit.co/b5401f0a7ff5ea4761953ab50381d0ed "
+  }
+}
